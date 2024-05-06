36041a74cf06bfacf98616df481107fa8e3367ac
Syahridan Asla <69499578+muxika21@users.noreply.github.com>
2024-05-06 22:19:31 +0800

    Create name.py

---
+++ name.py
@@ -0,0 +1,9 @@
+def main():
+    # Prompt the user to enter their name
+    name = input("Enter your name: ")
+
+    # Greet the user
+    print("Hello, " + name + "! Welcome to Python.")
+
+if __name__ == "__main__":
+    main()
